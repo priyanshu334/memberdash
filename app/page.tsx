59ae986c5cd7cf749f8eada73aa4f9d768b0c0f5
priyanshu334 <99priyanshu33@gmail.com>
2025-04-19 19:18:03 +0530

    oo

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
     setError('');
 
     try {
-      const res = await fetch(`https://backend.nurdcells.com/5000/api/members/login`, {
+      const res = await fetch(`https://backend.nurdcells.com/api/members/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ phone, password })
